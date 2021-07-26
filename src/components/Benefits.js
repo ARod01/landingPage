2d1319a82840e9fada12970f767ab1b735102af7
alfre <alfrerodriguez01@gmail.com>
2021-07-26 16:31:02 -0500

    section signup

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -10,15 +10,15 @@ const Benefits = () =>{
                 <div className="benefits__container-article">
                     <article className="benefits__article">
                         <div className="benefits__article-img"></div>
-                        <p className="benefits__article-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel culpa</p>
+                        <p className="benefits__article-desc">In-house SEO specialists working on a large projects</p>
                     </article>
                     <article className="benefits__article">
                         <div className="benefits__article-img"></div>
-                        <p className="benefits__article-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel culpa</p>
+                        <p className="benefits__article-desc">Licensed SEO experts and CEO enthusiasts</p>
                     </article>
                     <article className="benefits__article">
                         <div className="benefits__article-img"></div>
-                        <p className="benefits__article-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel culpa</p>
+                        <p className="benefits__article-desc">SEO geeks</p>
                     </article>
                 </div>
             </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Image1 from "../assets/image/Iphone.png";
+import Image2 from "../assets/image/Mobile.png";
+
+import "../assets/style/components/signup.scss"
+
 
 const SignUp = () =>{
     return(
         <>
-        <h1>Soy el SignUp</h1>
+        <section className="signup">
+            <div className="signup__blue">
+                <div className="signup__blue-img">
+                    <img src={Image1} alt="cell-phone1"/>
+                </div>
+                <div className="signup__blue-text">
+                    <h2>Sign up for the course now</h2>
+                    <p>and get a PDF course report & certificate via email</p>
+                    <button>Register</button>
+                </div>
+                <div className="signup__blue-img">
+                    <img src={Image2} alt="cell-phone2"/>
+                </div>
+            </div>
+            <div className="signup__white">
+            </div>
+        </section>
         </>
     )
 }
