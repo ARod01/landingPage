e2b3b7e399e319de206537e383a70b2273251a47
alfre <alfrerodriguez01@gmail.com>
2021-07-26 15:06:29 -0500

    section Benefits

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import "../assets/style/components/benefit.scss";
 
 const Benefits = () =>{
     return(
         <>
-        <h1>Soy el Benefits</h1>
+        <section className="benefits">
+            <div className="benefits__container">
+                <h2 className="benefits__container-title">Who will benefit</h2>
+                <div className="benefits__container-article">
+                    <article className="benefits__article">
+                        <div className="benefits__article-img"></div>
+                        <p className="benefits__article-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel culpa</p>
+                    </article>
+                    <article className="benefits__article">
+                        <div className="benefits__article-img"></div>
+                        <p className="benefits__article-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel culpa</p>
+                    </article>
+                    <article className="benefits__article">
+                        <div className="benefits__article-img"></div>
+                        <p className="benefits__article-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel culpa</p>
+                    </article>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
