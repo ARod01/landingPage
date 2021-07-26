5f3cc7f268a7124709106f8ea418fd7af05af407
alfre <alfrerodriguez01@gmail.com>
2021-07-26 17:20:26 -0500

    add cursor pointer and bootstrap librery

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Navbar, Nav} from 'react-bootstrap';
 import "../assets/style/components/header.scss";
 
 const Header = () =>{
     return(
         <>
         <header className="header">
-            <nav className="header__nav">
-
-            </nav>
+        <Navbar collapseOnSelect expand="md" variant="dark" className="navbar">
+                <Navbar.Brand href="#home">SEO Solution</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="item">
+                    <Nav className="mr-auto">
+                        <a className="navbar__item" href=".header">About</a>
+                        <a className="navbar__item" href=".header">Tutors</a>
+                        <a className="navbar__item" href=".header">Programs</a>
+                        <a className="navbar__item" href=".header">Location</a>
+                        <a className="navbar__item" href=".header">Book a place</a>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
         </header>
         </>
     )
